fc3b915a6973c9a4fa5d06bd4492f20398a26ef6
Martijn Cuppens <martijn.cuppens@intracto.com>
2018-12-30 18:00:09 +0200

    Implement our Stylelint configs for css and scss.

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # stylelint-config-bootstrap
 
-Stylelint config for Bootstrap
+Default stylelint config used by Bootstrap.
+
+## Installation
+
+```bash
+npm install stylelint-config-bootstrap --save-dev
+# Or with yarn:
+yarn add stylelint-config-bootstrap --dev
+```
+
+## Usage
+
+We provide a config for both CSS and SCSS. You can choose which one you would like to extend in your configuration:
+
+### CSS
+
+```json
+{
+  "extends": "stylelint-config-bootstrap/css"
+}
+```
+
+### SCSS
+
+```json
+{
+  "extends": "stylelint-config-bootstrap/scss"
+}
+```
---
+++ scss/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  'extends': [
+    'stylelint-config-recommended-scss',
+    '../css'
+  ],
+  'rules': {
+    'at-rule-no-unknown': null,
+    'property-blacklist': [
+      'border-radius',
+      'border-top-left-radius',
+      'border-top-right-radius',
+      'border-bottom-right-radius',
+      'border-bottom-left-radius',
+      'transition'
+    ],
+    'scss/at-function-named-arguments': 'never',
+    'scss/at-function-parentheses-space-before': 'never',
+    'scss/at-import-no-partial-leading-underscore': true,
+    'scss/at-mixin-argumentless-call-parentheses': null,
+    'scss/at-mixin-named-arguments': null,
+    'scss/at-mixin-parentheses-space-before': 'never',
+    'scss/at-rule-no-unknown': true,
+    'scss/dollar-variable-colon-space-after': 'at-least-one-space',
+    'scss/dollar-variable-colon-space-before': 'never',
+    'scss/dollar-variable-default': [
+      true,
+      {
+        'ignore': 'local'
+      }
+    ],
+    'scss/dollar-variable-no-missing-interpolation': true,
+    'scss/media-feature-value-dollar-variable': null,
+    'scss/no-duplicate-dollar-variables': [
+      null,
+      {
+        'ignoreInsideAtRules': [
+          'if',
+          'mixin'
+        ]
+      }
+    ],
+    'scss/operator-no-newline-after': true,
+    'scss/operator-no-newline-before': true,
+    'scss/operator-no-unspaced': true
+  }
+};
