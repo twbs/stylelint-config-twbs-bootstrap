5549e9561f1cd0d442a7346b4141dbae6a47ce9e
XhmikosR <xhmikosr@gmail.com>
2019-10-24 09:47:42 +0300

    Add `scss/no-duplicate-mixins: true`

--- scss/index.js
+++ scss/index.js
@@ -33,6 +33,7 @@ module.exports = {
         ]
       }
     ],
+    'scss/no-duplicate-mixins': true,
     'scss/operator-no-newline-after': true,
     'scss/operator-no-newline-before': true,
     'scss/operator-no-unspaced': true
