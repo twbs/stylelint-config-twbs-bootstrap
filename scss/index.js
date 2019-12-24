3982cc31b8da0edf6e386e040cba10fb5978cefb
Martijn Cuppens <martijn.cuppens@gmail.com>
2019-12-24 13:04:43 +0100

    Variable/function name rules (#38)
    - Variable names should start with a lowercase letter and only contain lowercase letters, numbers and dashes.
    - Function names should start with a lowercase letter and only contain lowercase letters and dashes.

--- scss/index.js
+++ scss/index.js
@@ -8,6 +8,7 @@ module.exports = {
   'rules': {
     'at-rule-no-unknown': null,
     'scss/at-function-named-arguments': 'never',
+    "scss/at-function-pattern": "^[a-z][a-z-]*$",
     'scss/at-function-parentheses-space-before': 'never',
     'scss/at-import-no-partial-leading-underscore': true,
     'scss/at-mixin-argumentless-call-parentheses': 'always',
@@ -17,6 +18,7 @@ module.exports = {
     'scss/dollar-variable-colon-space-after': 'at-least-one-space',
     'scss/dollar-variable-colon-space-before': 'never',
     'scss/dollar-variable-no-missing-interpolation': true,
+    "scss/dollar-variable-pattern": "^[a-z][a-z-\\d]*$",
     'scss/media-feature-value-dollar-variable': null,
     'scss/no-duplicate-dollar-variables': [
       null,
