f0b408155ada346f7210fd7a3c706dae85973832
XhmikosR <xhmikosr@gmail.com>
2020-09-15 11:59:41 +0300

    Enable a few more rules: (#70)
    * scss/at-import-partial-extension
    * scss/dimension-no-non-numeric-values
    * scss/selector-no-redundant-nesting-selector

--- scss/index.js
+++ scss/index.js
@@ -11,10 +11,12 @@ module.exports = {
     'scss/at-function-pattern': '^[a-z][a-z-]*$',
     'scss/at-function-parentheses-space-before': 'never',
     'scss/at-import-no-partial-leading-underscore': true,
+    'scss/at-import-partial-extension': 'never',
     'scss/at-mixin-argumentless-call-parentheses': 'always',
     'scss/at-mixin-named-arguments': null,
     'scss/at-mixin-parentheses-space-before': 'never',
     'scss/at-rule-no-unknown': true,
+    'scss/dimension-no-non-numeric-values': true,
     'scss/dollar-variable-colon-space-after': 'at-least-one-space',
     'scss/dollar-variable-colon-space-before': 'never',
     'scss/dollar-variable-no-missing-interpolation': true,
@@ -32,6 +34,7 @@ module.exports = {
     'scss/no-duplicate-mixins': true,
     'scss/operator-no-newline-after': true,
     'scss/operator-no-newline-before': true,
-    'scss/operator-no-unspaced': true
+    'scss/operator-no-unspaced': true,
+    'scss/selector-no-redundant-nesting-selector': true
   }
 };
