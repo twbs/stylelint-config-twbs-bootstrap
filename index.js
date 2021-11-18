5c3e56a194b7c7621f872ac647265bda92fcbe31
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2021-11-19 00:04:57 +0100

    Remove rules that are now same in stylelint-config-standard-scss

--- index.js
+++ index.js
@@ -265,22 +265,12 @@ module.exports = {
       'files': '**/*.scss',
       'extends': ['stylelint-config-standard-scss'],
       'rules': {
-        'at-rule-no-unknown': null,
         'no-invalid-position-at-import-rule': null,
         'scss/at-function-named-arguments': 'never',
-        'scss/at-function-pattern': '^[a-z][a-z-]*$',
-        'scss/at-function-parentheses-space-before': 'never',
-        'scss/at-import-no-partial-leading-underscore': true,
-        'scss/at-import-partial-extension': 'never',
         'scss/at-mixin-argumentless-call-parentheses': 'always',
         'scss/at-mixin-named-arguments': null,
-        'scss/at-mixin-parentheses-space-before': 'never',
-        'scss/at-rule-no-unknown': true,
         'scss/dimension-no-non-numeric-values': true,
         'scss/dollar-variable-colon-space-after': 'at-least-one-space',
-        'scss/dollar-variable-colon-space-before': 'never',
-        'scss/dollar-variable-no-missing-interpolation': true,
-        'scss/dollar-variable-pattern': '^[a-z][a-z-\\d]*$',
         'scss/media-feature-value-dollar-variable': null,
         'scss/no-duplicate-dollar-variables': [
           null,
@@ -291,10 +281,6 @@ module.exports = {
             ]
           }
         ],
-        'scss/no-duplicate-mixins': true,
-        'scss/operator-no-newline-after': true,
-        'scss/operator-no-newline-before': true,
-        'scss/operator-no-unspaced': true,
         'scss/selector-no-redundant-nesting-selector': true
       }
     }
