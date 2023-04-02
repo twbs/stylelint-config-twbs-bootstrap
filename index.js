e597fc577ced2d87b63a64e08fcb65910540860a
XhmikosR <xhmikosr@gmail.com>
2023-04-02 09:54:53 +0300

    Replace deprecated rules with prettier

--- index.js
+++ index.js
@@ -5,20 +5,17 @@ module.exports = {
     'stylelint-config-standard',
     'stylelint-config-recess-order'
   ],
+  'plugins': [
+    'stylelint-prettier'
+  ],
   'rules': {
     'alpha-value-notation': null,
     'at-rule-empty-line-before': null,
-    'at-rule-name-space-after': 'always',
-    'at-rule-semicolon-space-before': 'never',
-    'block-closing-brace-empty-line-before': null,
-    'block-closing-brace-newline-after': null,
-    'block-opening-brace-space-before': null,
     'color-function-notation': null,
     'color-named': 'never',
     'custom-property-empty-line-before': null,
     'custom-property-pattern': null,
     'declaration-block-no-redundant-longhand-properties': null,
-    'declaration-block-semicolon-newline-before': 'never-multi-line',
     'declaration-empty-line-before': null,
     'declaration-no-important': true,
     'font-weight-notation': [
@@ -30,16 +27,11 @@ module.exports = {
       }
     ],
     'function-url-no-scheme-relative': true,
-    'max-empty-lines': 2,
-    'max-line-length': null,
     'media-feature-range-notation': null,
     'no-descending-specificity': null,
-    'number-leading-zero': 'never',
     'number-max-precision': null,
+    'prettier/prettier': true,
     'rule-empty-line-before': null,
-    'selector-list-comma-newline-before': 'never-multi-line',
-    'selector-list-comma-space-after': 'always-single-line',
-    'selector-list-comma-space-before': 'never-single-line',
     'selector-max-attribute': 2,
     'selector-max-class': 4,
     'selector-max-combinators': 4,
@@ -49,11 +41,7 @@ module.exports = {
     'selector-max-type': 2,
     'selector-max-universal': 1,
     'selector-no-qualifying-type': true,
-    'selector-not-notation': null, // TODO enable this later
-    'unicode-bom': 'never',
-    'value-list-comma-newline-after': 'never-multi-line',
-    'value-list-comma-newline-before': 'never-multi-line',
-    'value-list-comma-space-after': 'always'
+    'selector-not-notation': null // TODO enable this later
   },
   'overrides': [
     {
@@ -82,6 +70,7 @@ module.exports = {
         'scss/media-feature-value-dollar-variable': null,
         // Disable since node-sass is still supported as a builder and it doesn't support the @use syntax
         'scss/no-global-function-names': null,
+        'scss/operator-no-newline-after': null,
         'scss/selector-no-redundant-nesting-selector': true
       }
     }
