7023ce46962de0155486d3469e928d0d61b57ccf
XhmikosR <xhmikosr@gmail.com>
2023-04-03 09:32:53 +0300

    Replace deprecated rules with stylelint-stylistic

--- index.js
+++ index.js
@@ -5,20 +5,17 @@ module.exports = {
     'stylelint-config-standard',
     'stylelint-config-recess-order'
   ],
+  'plugins': [
+    'stylelint-stylistic'
+  ],
   'rules': {
     'alpha-value-notation': null,
     'at-rule-empty-line-before': null,
-    'at-rule-name-space-after': 'always',
-    'at-rule-semicolon-space-before': 'never',
-    'block-closing-brace-empty-line-before': null,
-    'block-closing-brace-newline-after': null,
-    'block-opening-brace-space-before': null,
     'color-function-notation': null,
     'color-named': 'never',
     'custom-property-empty-line-before': null,
     'custom-property-pattern': null,
     'declaration-block-no-redundant-longhand-properties': null,
-    'declaration-block-semicolon-newline-before': 'never-multi-line',
     'declaration-empty-line-before': null,
     'declaration-no-important': true,
     'font-weight-notation': [
@@ -30,16 +27,10 @@ module.exports = {
       }
     ],
     'function-url-no-scheme-relative': true,
-    'max-empty-lines': 2,
-    'max-line-length': null,
     'media-feature-range-notation': null,
     'no-descending-specificity': null,
-    'number-leading-zero': 'never',
     'number-max-precision': null,
     'rule-empty-line-before': null,
-    'selector-list-comma-newline-before': 'never-multi-line',
-    'selector-list-comma-space-after': 'always-single-line',
-    'selector-list-comma-space-before': 'never-single-line',
     'selector-max-attribute': 2,
     'selector-max-class': 4,
     'selector-max-combinators': 4,
@@ -50,10 +41,18 @@ module.exports = {
     'selector-max-universal': 1,
     'selector-no-qualifying-type': true,
     'selector-not-notation': null, // TODO enable this later
-    'unicode-bom': 'never',
-    'value-list-comma-newline-after': 'never-multi-line',
-    'value-list-comma-newline-before': 'never-multi-line',
-    'value-list-comma-space-after': 'always'
+    'stylistic/at-rule-name-space-after': 'always',
+    'stylistic/at-rule-semicolon-space-before': 'never',
+    'stylistic/declaration-block-semicolon-newline-before': 'never-multi-line',
+    'stylistic/max-empty-lines': 2,
+    'stylistic/number-leading-zero': 'never',
+    'stylistic/selector-list-comma-newline-before': 'never-multi-line',
+    'stylistic/selector-list-comma-space-after': 'always-single-line',
+    'stylistic/selector-list-comma-space-before': 'never-single-line',
+    'stylistic/unicode-bom': 'never',
+    'stylistic/value-list-comma-newline-after': 'never-multi-line',
+    'stylistic/value-list-comma-newline-before': 'never-multi-line',
+    'stylistic/value-list-comma-space-after': 'always'
   },
   'overrides': [
     {
